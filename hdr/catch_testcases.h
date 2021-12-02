fdc970ee4a2bf1ad4114a7be29321bf6ec9adecb
trkinvincible <trkinvincible85@yahoo.com>
2021-12-03 02:08:24 +0800

    testcases updated.

---
+++ hdr/catch_testcases.h
@@ -0,0 +1,53 @@
+#pragma once
+
+// validate output
+#define CATCH_CONFIG_MAIN
+#include "../hdr/catch.hpp"
+
+std::unique_ptr<Task> Test_Function(std::string filename)
+{
+    std::unique_ptr<RkConfig> config = std::make_unique<RkConfig>([filename](config_data &d, boost::program_options::options_description &desc){
+        desc.add_options()
+                ("-b", boost::program_options::value<std::uint16_t>(&d.bins)->default_value(280), "# of bins in histogram (int)")
+                ("-min", boost::program_options::value<double>(&d.min)->default_value(0.0), "Value at low end of histogram. Defaults to lowest value found in input nrrd. (double)")
+                ("-max", boost::program_options::value<double>(&d.max)->default_value(280.0), "Value at high end of histogram. Defaults to highest value found in input nrrd. (double)")
+                ("-t", boost::program_options::value<uint8_t>(&d.type)->default_value(1), "type to use for bins in output histogram; default: \"uint\"")
+                ("-i", boost::program_options::value<std::string>(&d.input_file_name)->default_value(filename.data()), "input nrrd")
+                ("-o", boost::program_options::value<std::string>(&d.output_file_name)->default_value("../solution.txt"), "solution file");
+    });
+
+    try {
+        char* a[] = {"Ex2", "-i", filename.data()};
+        config->parse(5, a);
+    }
+    catch(std::exception const& e) {
+        std::cout << e.what();
+    }
+
+    std::unique_ptr<Task> task = std::make_unique<ComputeHistogram>(config);
+    task->Compute();
+
+    return task;
+}
+
+TEST_CASE("Validate Output with Total Pixels")
+{
+    // check /res/short-gzip.nrrd
+    const auto& t1 = Test_Function("../res/short-gzip.nrrd");
+    // check if solution file generated
+    std::ifstream solution_file("../solution.txt", std::ios::in);
+    if (!solution_file.is_open())
+        REQUIRE(false);
+    auto o1 = (256*256*130);
+    REQUIRE(t1->OutputVal() == o1);
+
+    // check /res/uchar-gzip.nrrd
+    const auto& t2 = Test_Function("../res/uchar-gzip.nrrd");
+    auto o2 = (215*215*167);
+    REQUIRE(t2->OutputVal() == o2);
+
+    // check /res/uchar-raw.nrrd
+    const auto& t3 = Test_Function("../res/uchar-raw.nrrd");
+    auto o3 = (3*128*128);
+    REQUIRE(t3->OutputVal() == o3);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -38,43 +38,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 #else
-// validate output
-#define CATCH_CONFIG_MAIN
-#include "../hdr/catch.hpp"
 
-TEST_CASE("Validate Output with Total Pixels")
-{
-    std::unique_ptr<RkConfig> config = std::make_unique<RkConfig>([](config_data &d, boost::program_options::options_description &desc){
-        desc.add_options()
-                ("-b", boost::program_options::value<std::uint16_t>(&d.bins)->default_value(256), "# of bins in histogram (int)")
-                ("-min", boost::program_options::value<double>(&d.min)->default_value(0.0), "Value at low end of histogram. Defaults to lowest value found in input nrrd. (double)")
-                ("-max", boost::program_options::value<double>(&d.max)->default_value(255.0), "Value at high end of histogram. Defaults to highest value found in input nrrd. (double)")
-                ("-t", boost::program_options::value<uint8_t>(&d.type)->default_value(1), "type to use for bins in output histogram; default: \"uint\"")
-                ("-i", boost::program_options::value<std::string>(&d.input_file_name)->default_value("../res/sample.nrrd"), "input nrrd")
-                ("-o", boost::program_options::value<std::string>(&d.output_file_name)->default_value("../solution.txt"), "solution file");
-    });
+#include "../hdr/catch_testcases.h"
 
-    try {
-
-        //-i ../res/sample.nrrd -b 256
-        char* a[] = {"Ex2","-i","../res/sample.nrrd","-b","256"};
-        config->parse(5, a);
-    }
-    catch(std::exception const& e) {
-
-        std::cout << e.what();
-    }
-
-    std::unique_ptr<Task> task = std::make_unique<ComputeHistogram>(config);
-    task->Compute();
-
-    // check if solution file generated
-    std::ifstream solution_file("../solution.txt", std::ios::in);
-    if (!solution_file.is_open())
-        REQUIRE(false);
-
-    // check if all pixels accounted
-    auto t = (215*215*167);
-    REQUIRE(task->OutputVal() == t);
-}
 #endif
