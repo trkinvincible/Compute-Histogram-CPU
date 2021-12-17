fe0155c5b2d6c17b3f622c5fd799647134e9fbfa
trkinvincible <trkinvincible85@yahoo.com>
2021-12-17 19:26:19 +0800

    milliseconds

--- src/main.cpp
+++ src/main.cpp
@@ -36,8 +36,8 @@ int main(int argc, char *argv[])
     }
 
     auto end = std::chrono::high_resolution_clock::now();
-    auto diff = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
-    std::cout << "total runtime : " << diff.count() << " microseconds." << std::endl;
+    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+    std::cout << "total runtime : " << diff.count() << " milliseconds." << std::endl;
 
     return 0;
 }
